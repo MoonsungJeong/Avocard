c99222b7866b6227b55ba7c137b4be35212cf649
Moonsung <gmsj159@gmail.com>
2022-03-21 23:19:09 +1000

    initialization

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hi");
+const express = require("express");
+
+const server = express();
+const port = 8080;
+
+server.listen(port, "192.168.1.223", () => {
+    console.log("Backend listening on http://localhost:" + port);
+});
+
+server.get("/", (req,res) => {
+    console.log("ok");
+});
