d1e800a46a22f91b5835814d51a9cbdf40345834
Moonsung <gmsj159@gmail.com>
2022-03-23 23:51:52 +1000

    backend test2

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const express = require("express");
 const server = express();
 const port = 8080;
 
-server.listen(port, "192.168.1.223", () => {
-    console.log("Backend listening on http://localhost:" + port);
-});
-
 server.get("/", (req,res) => {
     console.log("ok");
 });
@@ -16,3 +12,7 @@ server.use(express.static("frontend"));
 
 const userController = require("./backend/controllers/userController");
 server.use("/api", userController);
+
+server.listen(port, "192.168.1.223", () => {
+    console.log("Backend listening on http://localhost:" + port);
+});
